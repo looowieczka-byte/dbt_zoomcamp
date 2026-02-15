3b2965e362a1493a35d832dc8c71e70942e71218
looowieczka@gmail.com <looowieczka@gmail.com>
2026-02-15 21:00:15 +0000

    staging+ intermediate

---
+++ models/staging/stg_yellow_tripdata.sql
@@ -0,0 +1,25 @@
+select
+    cast (vendorid as int) as vendor_id,
+    cast (ratecodeid as int) as rate_code_id,
+    cast (pulocationid as int) as pickup_location_id,
+    cast (dolocationid as int) as dropoff_location_id,
+    -- timestamps
+    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    --trip info
+    store_and_fwd_flag,
+    cast (passenger_count as int) as passenger_count,
+    cast (trip_distance as numeric) as trip_distance,
+    1 as trip_type, -- yellow taxis can only be street-hail(trip_type=1)
+    --payment info
+    cast (fare_amount as numeric) as fare_amount,
+    cast (extra as numeric) as extra,
+    cast (mta_tax as numeric) as mta_tax,
+    cast (tip_amount as numeric) as tip_amount,
+    cast (tolls_amount as numeric) as tolls_amount,
+    cast (improvement_surcharge as numeric) as improvement_surcharge,
+    0 as ehail_fee, --yellow taxis do not have ehail fee
+    cast (total_amount as numeric) as total_amount,
+    cast (payment_type as int) as payment_type
+ from {{source('raw_data', 'yellow_taxi_trip_records_external_table')}}
+ where vendorid is not null
